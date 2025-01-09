6bde3f2b0e83bae1a3f263f0e12759219a10c710
agorbit <alberto.garcia@orbit.es>
2025-01-09 17:30:25 +0100

    arreglo fecha

--- models/imputaciones.py
+++ models/imputaciones.py
@@ -58,7 +58,7 @@ class Imputaciones (models.Model):
         self.fecha_final = datetime.today()
         FechaInicial = self.fecha_inicio
         FechaFinal = self.fecha_final
-        if FechaInicial != "" and FechaFinal != "":
+        if str(FechaInicial) != "" and str(FechaFinal) != "":
             diferencia = FechaFinal - FechaInicial
             self.tiempo = diferencia.total_seconds()/3600
             self.tiempo_realizado = self.tiempo * self.factor
@@ -103,24 +103,4 @@ class Imputaciones (models.Model):
                 }
                 
                 self.env(['account.analytic.line']).create(vals)
-        
-
-class Actividades (models.Model):
-    _name = 'actividades'
-
-    name = fields.Char('name')
-    company_id = fields.Many2one('res.company',string='Company', store=True, readonly=True,default=lambda self: self.env.company)
-    user_id = fields.Many2one('res.users', string='Assigned to', store=True, readonly=False,default=lambda self: self.env.user and self.env.user.id or False )
-    ticket = fields.Char('ticket')
-    partner_id = fields.Many2one('res.partner', string='partner')
-    case_id = fields.Many2one('helpdesk.ticket', string='Caso')
-    project_id = fields.Many2one('project.project', string='Proyecto')
-    task_id = fields.Many2one('project.task', string='Tarea')
-    descripcion = fields.Html('descripcion')    
-    cliente = fields.Boolean('cliente')
-    state = fields.Selection([
-        ('0', 'Borrador'),
-        ('1', 'Pendiente'),
-        ('2', 'En curso'),
-        ('3', 'Finalizado')
-    ],default='0' ,string='Estado')
+    
